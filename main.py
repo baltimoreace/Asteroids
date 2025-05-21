6bafac41ddb86640a373156fce468e4784ab5ef6
baltimoreace <baltimoreace@gmail.com>
2025-05-20 20:13:30 -0400

    FPS Update

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -11,12 +14,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill("black")
-        pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+    
+        screen.fill("black")
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
