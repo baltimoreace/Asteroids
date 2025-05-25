2b86f26d1d1258c1b2f036a237ff438723523172
baltimoreace <baltimoreace@gmail.com>
2025-05-25 14:57:38 -0400

    Forward and Backward Movement

--- player.py
+++ player.py
@@ -2,11 +2,15 @@ import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
         self.rotation = 0
         super().__init__(x, y, PLAYER_RADIUS)
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
     
     # in the player class
@@ -22,14 +26,18 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt    
+        self.rotation += PLAYER_TURN_SPEED * dt  
+  
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(dt)
-        if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
-            
